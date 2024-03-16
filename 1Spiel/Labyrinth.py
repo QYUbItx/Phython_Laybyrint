8edc45a0340c322c35777430c6e7b89bb27cd250
QYUbItx <145557846+QYUbItx@users.noreply.github.com>
2024-03-16 15:41:51 +0100

    .

--- 1Spiel/Labyrinth.py
+++ 1Spiel/Labyrinth.py
@@ -6,22 +6,17 @@ arcade.set_background_color(arcade.color.AERO_BLUE)
 
 block_liste = arcade.SpriteList()
 
-block_x_koordinaten = [25]
-block_y_koordinaten = [25]
+block_x_koordinaten = [25, 50]
+block_y_koordinaten = [25, 50] 
 
 for i in block_x_koordinaten:
-    block_liste[i].center_x = block_x_koordinaten[i]
-    block_liste[i].center_y = block_y_koordinaten[i]
-
-
-    
-block = arcade.Sprite("texturen/block.png")
-block.center_x = 25
-block.center_y = 25
-block_liste.append(block)
+    block = arcade.Sprite("texturen/block.png")
+    block.center_x = block_x_koordinaten[i - 1]
+    block.center_y = block_y_koordinaten[i - 1]
+    block_liste.append(block)
 
 arcade.start_render()
 block_liste.draw()
 arcade.finish_render()
 
-arcade.run()###
+arcade.run()
