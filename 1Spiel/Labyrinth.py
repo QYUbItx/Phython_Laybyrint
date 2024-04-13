2da4c9a91e808b9c789a511a841566d2944ecace
QYUbItx <145557846+QYUbItx@users.noreply.github.com>
2024-04-13 16:01:36 +0200

    neue struktur

--- 1Spiel/Labyrinth.py
+++ 1Spiel/Labyrinth.py
@@ -1,101 +1,111 @@
 #ganzes arcade window zeug
 import arcade
-arcade.open_window(800, 600, "Labyrinth")
-arcade.set_background_color(arcade.color.AERO_BLUE)
-block_liste = arcade.SpriteList() #das wird gedrawd
 
 
 
-#listen der block koordinaten #TODO das ist quatsch, mit objekt ist besser
-block_x_kor = []
-block_y_kor = []
+
+class Labyrinth(arcade.Window):
+
+    #ist dafür da, um Linien aus Blöcken zu machen
+    class Pointer:
+        x = 0
+        y = 0
+
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+
+    def blockLine(self, x, y, length, horizontal):
+        pointer = self.Pointer(x, y)
+        self.createBlock(pointer.x, pointer.y, "block")
+        length -= 1
+        i = 0
+        if horizontal:  
+            while i < length:
+                pointer.x += 50
+                self.createBlock(pointer.x, pointer.y, "block")
+                i += 1
+        else:
+            while i < length:
+                pointer.y += 50
+                self.createBlock(pointer.x, pointer.y, "block") 
+                i += 1
 
 
+    #macht Sachen einfacher
+    def readebleBlockKor(self, int):
+        return int + 25
 
-#ist dafür da, um Linien aus Blöcken zu machen
-class Pointer:
-    x = 0
-    y = 0
+    #erstellt einen block und appendet ihn zur block liste
+    def createBlock(self, x, y, type):
+        block = arcade.Sprite("texturen/" + type + ".png")
+        block.center_x = self.readebleBlockKor(x)
+        block.center_y = self.readebleBlockKor(y)
+        self.block_liste.append(block)
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    
+
+    def __init__(self):
+        super().__init__(800, 600, "Labyrinth")
+        arcade.set_background_color(arcade.color.AERO_BLUE)
+
+        self.block_liste = arcade.SpriteList() #das wird gedrawd
+        self.entity_liste = arcade.SpriteList()
+
+
+        #Linien der Ränder
+        self.blockLine(0, 0, 16, True)
+        self.blockLine(0, 550, 16, True)
+        self.blockLine(0, 0, 12, False)
+        self.blockLine(750, 0, 12, False)
+
+        #andere Linien
+        self.blockLine(100, 450, 4, True)
+        self.blockLine(100, 300, 3, False)
+        self.blockLine(350, 350, 3, False)
+        self.blockLine(200, 300, 5, True)
+        self.blockLine(400, 450, 2, True)
+        self.blockLine(550, 450, 2, False)
+        self.blockLine(500, 350, 3, True)
+        self.blockLine(650, 200, 6, False)
+        self.blockLine(500, 150, 6, True)
+        self.blockLine(400, 50, 5, False)
+        self.blockLine(150, 150, 5, True)
+        self.blockLine(100, 150, 2, False)
+
+        #einzelne Blöcke
+        self.createBlock(250, 400, "block")
+        self.createBlock(300, 250, "block")
+        self.createBlock(200, 200, "block")
+        self.createBlock(200, 100, "block")
+        self.createBlock(300, 50, "block")
+        self.createBlock(550, 250, "block")
+        self.createBlock(450, 250, "block")
+        self.createBlock(500, 100, "block")
+        self.createBlock(600, 50, "block")
+        self.createBlock(100, 50, "block")
+
+
+        player = arcade.Sprite("texturen/player.png")
+        player.center_x = 325
+        player.center_y = 225
+        player.height = 50
+        player.width = 50
+        self.entity_liste.append(player)
+
+    def on_key_press(self, char, modifiers):
+        if char.w:
+            print("w")
         
-def appendKor(x, y):
-    block_x_kor.append(x)
-    block_y_kor.append(y)
 
-def blockLine(x, y, length, horizontal):
-    pointer = Pointer(x, y)
-    appendKor(pointer.x, pointer.y)
-    length -= 1
-    i = 0
-    if horizontal:  
-        while i < length:
-            pointer.x += 50
-            appendKor(pointer.x, pointer.y)
-            i += 1
-    else:
-        while i < length:
-            pointer.y += 50
-            appendKor(pointer.x, pointer.y) 
-            i += 1
 
-#Linien der Ränder
-blockLine(0, 0, 16, True)
-blockLine(0, 550, 16, True)
-blockLine(0, 0, 12, False)
-blockLine(750, 0, 12, False)
-
-#andere Linien
-blockLine(100, 450, 4, True)
-blockLine(100, 300, 3, False)
-blockLine(350, 350, 3, False)
-blockLine(200, 300, 5, True)
-blockLine(400, 450, 2, True)
-blockLine(550, 450, 2, False)
-blockLine(500, 350, 3, True)
-blockLine(650, 200, 6, False)
-blockLine(500, 150, 6, True)
-blockLine(400, 50, 5, False)
-blockLine(150, 150, 5, True)
-blockLine(100, 150, 2, False)
-
-#einzelne Blöcke
-appendKor(250, 400)
-appendKor(300, 250)
-appendKor(200, 200)
-appendKor(200, 100)
-appendKor(300, 50)
-appendKor(550, 250)
-appendKor(450, 250)
-appendKor(500, 100)
-appendKor(600, 50)
-appendKor(100, 50)
+    def on_draw(self):
+        self.clear()
+        self.block_liste.draw()
+        self.entity_liste.draw()
 
 
 
-#macht Sachen einfacher
-def readebleBlockKor(int):
-    return int + 25
 
-for i, elements in enumerate(block_x_kor):
-    block_x_kor[i] = readebleBlockKor(block_x_kor[i])
-    block_y_kor[i] = readebleBlockKor(block_y_kor[i])
-
-
-
-#macht objekte mit koordinaten in die blockliste
-for i, elements in enumerate(block_x_kor):
-    block = arcade.Sprite("texturen/block.png")
-    block.center_x = block_x_kor[i]
-    block.center_y = block_y_kor[i]
-    block_liste.append(block)
-
-
-
-#rendert und startet das Spiel
-arcade.start_render()
-block_liste.draw()
-arcade.finish_render()
+labyrinth = Labyrinth()
 arcade.run()
